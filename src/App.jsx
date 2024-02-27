20afbd772fa6e200e1b331c823dca0198721587c
RitickLath <ritick943lath@gmail.com>
2024-02-28 02:03:32 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Body from "./Components/Body";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "./Components/utils/firebase";
+
+const auth = getAuth(app);
 
 function App() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("you are logged in " + user);
+      } else {
+        console.log("you aren't");
+      }
+    });
+  }, []);
   return (
     <>
       <Body />
-      
     </>
   );
 }
