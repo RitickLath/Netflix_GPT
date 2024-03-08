0b87da851b08ef93ffc64ccb2d1756038fb0fbb2
RitickLath <ritick943lath@gmail.com>
2024-03-08 09:27:01 +0530

    styling fix

--- src/Components/MainUI/PrimaryUI/HeaderInside.jsx
+++ src/Components/MainUI/PrimaryUI/HeaderInside.jsx
@@ -26,7 +26,7 @@ const HeaderInside = () => {
   return (
     <div className="z-50 absolute top-0 w-full flex justify-between items-center px-8 pr-3 pt-4">
       <img
-        className=" w-[100px] bg-gradient-to-br from-black rounded-2xl"
+        className=" w-[100px]"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt=""
       />
