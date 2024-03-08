a4bc4dbe8597ddfc5782f95f9170c71bd00405f9
RitickLath <ritick943lath@gmail.com>
2024-03-08 07:33:48 +0530

    styling fix

--- src/Components/GPT Page/GptSearch.jsx
+++ src/Components/GPT Page/GptSearch.jsx
@@ -10,25 +10,30 @@ const GptSearch = () => {
   return (
     <div className="w-full bg-[#141414] text-white">
       <div className="w-full text-center">
-        <h1 className="text-[#D9232E] relative flex justify-center pt-12 px-4 text-3xl md:text-3xl lg:text-4xl font-bold">
+        <h1 className="text-[#D9232E] relative flex justify-center pt-12 px-4 text-2xl md:text-3xl lg:text-4xl font-bold">
           GPT-Powered Recommendations{" "}
           <div className="relative">
-            <FaInfo
-              onMouseEnter={() => {
-                setShowDiv(true);
-              }}
-              onMouseLeave={() => {
-                setShowDiv(false);
-              }}
-              className="relative mx-2 mt-[0.6rem] lg:mt-[1rem] text-[1.3rem] font-semibold text-white"
-            />
-            {showdiv && (
-              <p className="absolute bottom-7 right-6 w-[300px] rounded-md font-normal bg-white text-left text-xs text-black p-2">
-                Share your movie vision and get personalized recommendations.
-                Just describe your ideal pick, and our GPT-powered system will
-                find the perfect match for your taste.
-              </p>
-            )}
+            <div className="absolute">
+              <div className="relative">
+                <FaInfo
+                  onMouseEnter={() => {
+                    setShowDiv(true);
+                  }}
+                  onMouseLeave={() => {
+                    setShowDiv(false);
+                  }}
+                  className="relative mx-2 mt-[0.6rem] lg:mt-[1rem] text-[1.3rem] font-semibold text-white"
+                />
+                {showdiv && (
+                  <p className="absolute bottom-7 right-6 w-[300px] rounded-md font-normal bg-white text-left text-xs text-black p-2">
+                    Share your movie vision and get personalized
+                    recommendations. Just describe your ideal pick, and our
+                    GPT-powered system will find the perfect match for your
+                    taste.
+                  </p>
+                )}
+              </div>
+            </div>
           </div>
         </h1>
       </div>
